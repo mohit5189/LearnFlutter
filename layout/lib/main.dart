b462666568e579a7e35bfdd53441812e5799c965
Mohit Kumar <mohit.kumar@lalamove.com>
2019-12-26 13:04:46 +0530

    implement stateful widget

--- layout/lib/main.dart
+++ layout/lib/main.dart
@@ -28,13 +28,7 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          Column(children: <Widget>[Icon(Icons.star, color: Colors.red[500])]),
-          Column(children: <Widget>[
-            Text(
-              '41',
-              style: TextStyle(fontSize: 20.0),
-            )
-          ]),
+          FavoriteWidget(),
         ],
       ),
     );
@@ -118,5 +112,51 @@ class MyApp extends StatelessWidget {
       ],
     );
   }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
 
 }
+
